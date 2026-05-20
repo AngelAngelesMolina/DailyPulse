5201d3bfec212e3359f6b2bd73fd7f1a86ef2550
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-05-19 19:55:24 -0600

    ADD: Articles service

---
+++ composeApp/src/commonMain/kotlin/com/jaamcoding/dailypulse/articles/ArticlesService.kt
@@ -0,0 +1,22 @@
+package com.jaamcoding.dailypulse.articles
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+
+class ArticlesService(
+    private val client: HttpClient
+) {
+
+    private val country = "us"
+    private val category = "business"
+    private val apiKey = "b2139932c19c4656b80493696042a2c9"
+
+    suspend fun getArticles(): List<ArticleRaw> {
+        val response: ArticlesResponse =
+            client.get("https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=$apiKey")
+                .body()
+        return response.articles
+    }
+}
+
