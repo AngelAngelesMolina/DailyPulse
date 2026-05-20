4c51807584ebf1e647f0500cecb8bfd90f9762dc
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-05-19 20:33:40 -0600

    ADD: formating date

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -55,6 +55,9 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
+
+            implementation(libs.kotlinx.datetime)
+
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
--- composeApp/src/commonMain/kotlin/com/jaamcoding/dailypulse/articles/ArticlesUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/jaamcoding/dailypulse/articles/ArticlesUseCase.kt
@@ -1,5 +1,13 @@
 package com.jaamcoding.dailypulse.articles
 
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.todayIn
+import kotlin.math.abs
+import kotlin.time.Clock
+import kotlin.time.Instant
+
 class ArticlesUseCase(
     private val service: ArticlesService
 ) {
@@ -14,11 +22,24 @@ class ArticlesUseCase(
             Article(
                 title = articleRaw.title,
                 desc = articleRaw.description ?: "Click to find out more",
-                date = articleRaw.date,
+                date = getDaysAgoString(articleRaw.date),
                 imageUrl = articleRaw.imageUrl
                     ?: "https://image.cnbcfm.com/api/v1/image/107326078-1698758530118-gettyimages-1765623456-wall26362_igj6ehhp.jpeg?v=1698758587&w=1920&h=1080"
             )
         }
 
+    private fun getDaysAgoString(date: String): String {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val days = today.daysUntil(
+            Instant.parse(date).toLocalDateTime(TimeZone.currentSystemDefault()).date
+        )
 
+        val result = when {
+            abs(days) > 1 -> "${abs(days)} days ago"
+            abs(days) == 1 -> "Yesterday"
+            else -> "Today"
+        }
+
+        return result
+    }
 }
