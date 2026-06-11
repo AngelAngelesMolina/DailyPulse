cb753f0eefd341c549566bce288b6b95e2ebb668
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-06-10 21:23:19 -0600

    ADD: Source dao entities

---
+++ composeApp/src/commonMain/kotlin/com/jaamcoding/dailypulse/articles/data/SourceResponse.kt
@@ -0,0 +1,11 @@
+package com.jaamcoding.dailypulse.articles.data
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SourceResponse(
+    @SerialName("status")
+    val status : String,
+    @SerialName("sources")
+    val sources : List<SourceRaw>)
