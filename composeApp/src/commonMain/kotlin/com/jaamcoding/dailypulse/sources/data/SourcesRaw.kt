773de270a1ab02a410a32afbbdf99121656fb067
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2026-06-12 21:27:32 -0600

    ADD: Source data

--- composeApp/src/commonMain/kotlin/com/jaamcoding/dailypulse/sources/data/SourcesRaw.kt
+++ composeApp/src/commonMain/kotlin/com/jaamcoding/dailypulse/sources/data/SourcesRaw.kt
@@ -1,29 +1,19 @@
-package com.jaamcoding.dailypulse.articles.data
+package com.jaamcoding.dailypulse.sources.data
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class SourceRaw(
-    @SerialName("category")
-    val category: String,
     @SerialName("country")
-
     val country: String,
     @SerialName("description")
-
-    val description: String,
+    val description: String?,
     @SerialName("id")
-
     val id: String,
     @SerialName("language")
-
     val language: String,
     @SerialName("name")
-
-    val name: String,
-    @SerialName("url")
-
-    val url: String
+    val name: String
 )
 
